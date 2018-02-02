9d883cb88bf973a7685a715d01c764db0659d506
Diana Huang <dkh@edx.org>
2018-02-02 15:53:21 -0500

    Address code review feedback.

--- openedx/features/portfolio_project/views.py
+++ openedx/features/portfolio_project/views.py
@@ -2,19 +2,12 @@
 Portfolio views.
 """
 
-from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse
-from django.http import Http404
 from django.template.loader import render_to_string
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_control
-from django.views.decorators.csrf import ensure_csrf_cookie
 
-from lms.djangoapps.courseware.courses import get_course_with_access
 from lms.djangoapps.courseware.views.views import CourseTabView
 
-from openedx.core.djangoapps.plugin_api.views import EdxFragmentView
-from opaque_keys.edx.keys import CourseKey
 from util.views import ensure_valid_course_key
 
 from web_fragments.fragment import Fragment
@@ -24,7 +17,6 @@ class GenericTabView(CourseTabView):
     """
     Provides a blank page that acts as its own tab in courseware for displaying content.
     """
-    @method_decorator(ensure_csrf_cookie)
     @method_decorator(cache_control(no_cache=True, no_store=True, must_revalidate=True))
     @method_decorator(ensure_valid_course_key)
     def get(self, request, course_id, **kwargs):
@@ -41,7 +33,7 @@ class GenericTabView(CourseTabView):
             'course': course,
             'user': request.user,
             'uses_bootstrap': True,
-            'tab_name': 'portfolio_project',
+            'tab_name': tab,
         }
         html = render_to_string('portfolio_project/generic_tab.html', context)
         return Fragment(html)
